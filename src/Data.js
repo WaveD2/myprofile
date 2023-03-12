cd1dc8d4d8630741ce7dcbb23f51bf45ce86a135
WaveD <109639330+WaveD2@users.noreply.github.com>
2023-03-12 22:33:47 +0700

    update product

--- src/Data.js
+++ src/Data.js
@@ -24,6 +24,7 @@ import work4 from "./assets/work4.jpg";
 import work5 from "./assets/work5.jpg";
 import work6 from "./assets/work6.jpg";
 import work7 from "./assets/work7.png";
+import work8 from "./assets/work8.jpg";
 
 export const navLinks = ["home", "about", "skills", "portfolio", "contact"];
 
@@ -68,53 +69,61 @@ export const workImages = [
     img: work1,
     name: "project 1",
     category: "react",
-    link: " https://waved-admin.vercel.app/ ",
-    git: "https://github.com/WaveD2/admin",
+    link: "https://shop-huongmy.vercel.app",
+    git: "https://github.com/WaveD2/shop-huongmy",
   },
   {
     id: 2,
     img: work2,
     name: "project 2",
     category: "react",
-    link: " https://waved-education.vercel.app/",
-    git: "https://github.com/WaveD2/WaveD-education",
+    link: " https://waved-admin.vercel.app/ ",
+    git: "https://github.com/WaveD2/admin",
   },
   {
     id: 3,
     img: work3,
     name: "project 3",
     category: "react",
-    link: " https://wave-demonetflix.vercel.app/",
-    git: " https://github.com/WaveD2/demoNetflix",
+    link: " https://waved-education.vercel.app/",
+    git: "https://github.com/WaveD2/WaveD-education",
   },
   {
     id: 4,
     img: work4,
     name: "project 4",
     category: "react",
-    git: "https://github.com/WaveD2/WaveDpodcast",
-    link: "https://wave-podcast.vercel.app/",
+    link: " https://wave-demonetflix.vercel.app/",
+    git: " https://github.com/WaveD2/demoNetflix",
   },
   {
     id: 5,
     img: work5,
     name: "project 5",
-    link: "https://github.com/WaveD2/WaveD-Travel",
-    git: "https://waved2.github.io/WaveD-Travel/",
-    category: "html/css/js",
+    category: "react",
+    git: "https://github.com/WaveD2/WaveDpodcast",
+    link: "https://wave-podcast.vercel.app/",
   },
   {
     id: 6,
     img: work6,
     name: "project 6",
+    link: "https://github.com/WaveD2/WaveD-Travel",
+    git: "https://waved2.github.io/WaveD-Travel/",
+    category: "html/css/js",
+  },
+  {
+    id: 7,
+    img: work7,
+    name: "project 7",
     link: "https://wavedzingmp3.vercel.app/",
     git: "https://github.com/WaveD2/zingmp3",
     category: "react",
   },
   {
-    id: 7,
-    img: work7,
-    name: "project 6",
+    id: 8,
+    img: work8,
+    name: "project 8",
     link: "https://waved2.github.io/myCaro/",
     git: "https://github.com/WaveD2/myCaro",
     category: "html/css/js",
